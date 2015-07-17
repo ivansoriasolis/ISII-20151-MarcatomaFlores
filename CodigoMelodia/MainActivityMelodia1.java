8156d4af0f1ee18ee32e289d2cafc6786068a2a0
evelynmarflo <pericotita10@gmail.com>
2015-07-16 17:54:19 -0700

    avance del 17/07/2015 hoy

--- CodigoMelodia/MainActivityMelodia1.java
+++ CodigoMelodia/MainActivityMelodia1.java
@@ -400,6 +400,54 @@ _L5:
         }
           goto _L2
     }
+    class this._cls0 extends TimerTask
+{
+
+    final MainActivity this$0;
+
+    public void run()
+    {
+        boolean aflag[] = new boolean[18];
+        int i = 0;
+        do
+        {
+            if (i >= aflag.length)
+            {
+                if (!Arrays.equals(aflag, MainActivity.access$1(MainActivity.this)))
+                {
+                    MainActivity.access$3(MainActivity.this, MainActivity.access$2(MainActivity.this));
+                    runOnUiThread(new Runnable() {
+
+                        final MainActivity._cls1 this$1;
+
+                        public void run()
+                        {
+                            MainActivity.access$4(this$0).setImageBitmap(MainActivity.access$5(this$0));
+                        }
+
+            
+            {
+                this$1 = MainActivity._cls1.this;
+                super();
+            }
+                    });
+                }
+                MainActivity.access$6(MainActivity.this, aflag);
+                return;
+            }
+            aflag[i] = MainActivity.access$0(MainActivity.this)[i].isPlaying();
+            i++;
+        } while (true);
+    }
+
+
+    _cls1.this._cls1()
+    {
+        this$0 = MainActivity.this;
+        super();
+    }
+}
+
 
 
 
