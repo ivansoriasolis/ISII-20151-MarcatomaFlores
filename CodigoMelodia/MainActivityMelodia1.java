1c82716240fcd30bd500334916b7ceddd603c93e
evelynmarflo <pericotita10@gmail.com>
2015-07-16 19:45:32 -0700

    Casi vrficado ; No corre el funcion playsong

--- CodigoMelodia/MainActivityMelodia1.java
+++ CodigoMelodia/MainActivityMelodia1.java
@@ -75,6 +75,7 @@ _L4:
         if (i >= 18)
         {
             return bitmap;
+
         }
         break MISSING_BLOCK_LABEL_113;
 _L2:
@@ -194,7 +195,7 @@ _L2:
           goto _L4
     }
 
-    private void playNote(MediaPlayer mediaplayer)
+    private void playMusic(MediaPlayer mediaplayer)
     {
         mediaplayer.seekTo(0);
         mediaplayer.start();
@@ -214,7 +215,7 @@ _L2:
         }
     }
 
-    private void stopNote(MediaPlayer mediaplayer)
+    private void splayMusic(MediaPlayer mediaplayer)
     {
         mediaplayer.pause();
     }
@@ -399,6 +400,54 @@ _L5:
         }
           goto _L2
     }
+    class this._cls0 extends TimerTask
+{
+
+    final MainActivity this$0;
+
+    public void run()
+    {
+        boolean aflag[] = new boolean[18];
+        int i = 0;
+        do
+        {
+            if (i >= aflag.length)
+            {
+                if (!Arrays.equals(aflag, MainActivity.access$1(MainActivity.this)))
+                {
+                    MainActivity.access$3(MainActivity.this, MainActivity.access$2(MainActivity.this));
+                    runOnUiThread(new Runnable() {
+
+                        final MainActivity._cls1 this$1;
+
+                        public void run()
+                        {
+                            MainActivity.access$4(this$0).setImageBitmap(MainActivity.access$5(this$0));
+                        }
+
+            
+            {
+                this$1 = MainActivity._cls1.this;
+                super();
+            }
+                    });
+                }
+                MainActivity.access$6(MainActivity.this, aflag);
+                return;
+            }
+            aflag[i] = MainActivity.access$0(MainActivity.this)[i].isPlaying();
+            i++;
+        } while (true);
+    }
+
+
+    _cls1.this._cls1()
+    {
+        this$0 = MainActivity.this;
+        super();
+    }
+}
+
 
 
 
